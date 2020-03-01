f6946b6a3be6868bce8124659f6c1d06e3963c7c
SohIsa <guerra-isa@hotmail.com>
2020-03-01 09:23:09 -0300

    atividade

---
+++ longa ativ revisão.py
@@ -0,0 +1,34 @@
+#1
+'''def main ():
+    idade=int(input('em que ano você nasceu?'))
+    print (f'você tem {(2020-idade)*365} dias')
+main ()  '''
+#2
+'''def area (bas,alt):
+    return bas*alt/2
+def main():
+    base,altura=input('base e altura?').split()
+    base,altura=float(base), float(altura)  
+    print (area(base,altura))
+main()'''
+#3
+'''valor=float(input('diga um valor:'))
+if valor>10:
+    print ('É MAIOR QUE 10!')
+else:
+    print('NÃO É MAIOR QUE 10!')'''
+#4
+'''def preço(qtde):
+    if qtde<12:
+        return (qtde*1.30)
+    else:
+        return (qtde)
+def main():
+    maça=int(input('quantas maçãs?'))
+    print (preço(maça))
+main()'''
+#5
+
+#6
+#7
+
