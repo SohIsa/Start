090d2326bbf7ba97211bdce369fb1ac1ce161a85
Isa Polyana <60966483+SohIsa@users.noreply.github.com>
2020-03-16 10:54:30 -0300

    outro modo

--- Facd/recente/2.py
+++ Facd/recente/2.py
@@ -2,14 +2,25 @@ from listaa import Pilha
 from fili import Fila
 def main ():
     fome = Pilha()
+    grandfome = Pilha()
     emof = Fila()
-    for i in range(3):
+    fome.push('azul')
+    fome.push('rosa')
+    fome.push('roxo')
+    '''for i in range(3):
         a=(input('alimento:'))
         fome.push(a)
-        emof.novoValor(a)
-    print (fome.getPilha())
+        emof.novoValor(a)'''
+    while fome.getPilha():
+        grandfome.push(fome.pop())
+    print (grandfome.getPilha())
+    while grandfome.getPilha():
+        emof.novoValor(grandfome.pop())
+    print (emof.getFila())
+
+    '''print (fome.getPilha())
     print (fome.inverte())
     print (emof.getFila())
-    print (emof.verso())
+    print (emof.verso())'''
 
 main()
